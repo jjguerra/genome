271a757aee160b46ee07b0a3190e03757b6a7c22
Jorge Guerra C2B2 <jjg2188@columbia.edu>
2017-04-17 12:48:42 -0400

    refactoring vcftools automation script

--- other/vcftools_automate_prep.sh
+++ other/vcftools_automate_prep.sh
@@ -6,15 +6,8 @@ for member in ${dir}/*; do
     if [[ ${member} == "${dir}/Sample"* ]]; then
         echo "accessing ${member}"
         analysis_dir="${member}/analysis"
+        rm -rf analysis_dir/*.vcf.gz.tbi
         for vcf_file in ${analysis_dir}/*; do
-            if [[ ${vcf_file} == *"filtered.vcf.gz" ]]; then
-                echo "removing ${vcf_file}"
-                rm -rf ${vcf_file}
-            fi
-            if [[ ${vcf_file} == *".vcf.gz.tbi" ]]; then
-                echo "removing ${vcf_file}"
-                rm -rf ${vcf_file}
-            fi
             if [[ ${vcf_file} == *"Calls.vcf.gz" ]]; then
                 echo "processing ${vcf_file}"
                 vcf_filename="${vcf_file:0:(${#vcf_file}-3)}"
